35fd9c54011aa25714ce3d4e33a78459b73d56c4
abgeiger <30698092+abgeiger@users.noreply.github.com>
2020-03-24 14:06:37 -0500

    Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+    )
+  }
+}
+
+export default App
+
